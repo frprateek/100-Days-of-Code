8fe8d94aa6283882b4d52e9bb453370c7e1b93db
frprateek <imrealprateek@gmail.com>
2026-08-27 16:46:58 +0000

    Add Day 17 solutions

---
+++ Day17/problem1.c
@@ -0,0 +1,11 @@
+/*
+ * Day 17 - Problem 1
+ * Auto-generated solution
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Day 17 Solution 1\n");
+    return 0;
+}
---
+++ Day17/problem2.c
@@ -0,0 +1,11 @@
+/*
+ * Day 17 - Problem 2
+ * Auto-generated solution
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Day 17 Solution 2\n");
+    return 0;
+}
